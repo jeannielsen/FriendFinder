8449a3f0f11d9fbf4d973b7b4545ad79e27a2918
Nielsen <jeanielsen@comcast.net>
2019-04-05 20:50:04 -0700

    first push

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   // html GET Requests
   // In each of the below cases the user is shown an HTML page of content
 
-  app.get("/home", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
